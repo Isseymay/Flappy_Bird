8cfe1c0aa9218ee5a7db565caa139b33b401cb99
Isseymay <35818862+Isseymay@users.noreply.github.com>
2024-04-20 19:47:57 +1000

    Update Beginner_Ext1.py

--- Beginner_Ext1.py
+++ Beginner_Ext1.py
@@ -1,9 +1,10 @@
 # Making a simple flappy bird game with Pygame Zero
 # flappyBirdV2.1G
-# took away scoring, and game over. Positions just reset
+# took away scoring, and game over. code quits. Took away pipe groups
 
 # initialise Pygame Zero
 import pgzrun
+import sys
 
 # initialize constants
 WIDTH = 800
@@ -15,34 +16,35 @@ scrollSpeed = -1
     
 # make bird
 bird = Actor("bird")
-bird.x = WIDTH*0.18
-bird.y = HEIGHT*0.5
+bird.x = 160
+bird.y = 300
 
 # make pipes
-# pipe group 1
 topPipe1 = Actor("top")
-topPipe1.x = WIDTH*0.33
+topPipe1.x = 280
 topPipe1.y = 0
+
 bottomPipe1 = Actor("bottom")
-bottomPipe1.x = WIDTH*0.33
-bottomPipe1.y = topPipe1.height + gap
+bottomPipe1.x = 280
+bottomPipe1.y = 800
 
-# pipe group 2
+
 topPipe2 = Actor("top")
-topPipe2.x = WIDTH*0.66
-topPipe2.y = -(topPipe2.height*0.2)
+topPipe2.x = 545
+topPipe2.y = -200
+
 bottomPipe2 = Actor("bottom")
-bottomPipe2.x = WIDTH*0.66
-bottomPipe2.y = (topPipe2.height + topPipe2.y) + gap
+bottomPipe2.x = 545
+bottomPipe2.y = 560
 
 
-# pipe group 3
 topPipe3 = Actor("top")
-topPipe3.x = WIDTH
-topPipe3.y = -(topPipe3.height*0.4)
+topPipe3.x = 810
+topPipe3.y = -120
+
 bottomPipe3 = Actor("bottom")
-bottomPipe3.x = WIDTH
-bottomPipe3.y = (topPipe3.height + topPipe3.y) + gap
+bottomPipe3.x = 810
+bottomPipe3.y = 710
 
 # draw everything to screen
 def draw():
@@ -61,103 +63,80 @@ def update():
     # updating bird
     bird.y = bird.y + 1
 
-    # updating 1st pipe group
+    # updating 1st pipe 
     topPipe1.x = topPipe1.x + scrollSpeed
-    bottomPipe1.x = bottomPipe1.x + scrollSpeed
-    if topPipe1.x < (0-topPipe1.width):
+    if topPipe1.x < -87:
         topPipe1.x = WIDTH
+        
+    # updating 2nd pipe 
+    bottomPipe1.x = bottomPipe1.x + scrollSpeed
+    if bottomPipe1.x < -87:
         bottomPipe1.x = WIDTH
 
-    # updating 2nd pipe group
+    # updating 3rd pipe 
     topPipe2.x = topPipe2.x + scrollSpeed
-    bottomPipe2.x = bottomPipe2.x + scrollSpeed
-    if topPipe2.x < (0-topPipe2.width):
+    if topPipe2.x < -87:
         topPipe2.x = WIDTH
+        
+    # updating 4th pipe 
+    bottomPipe2.x = bottomPipe2.x + scrollSpeed
+    if bottomPipe2.x < -87:
         bottomPipe2.x = WIDTH
 
-    # updating 3rd pipe group
+    # updating 5th pipe 
     topPipe3.x = topPipe3.x + scrollSpeed
-    bottomPipe3.x = bottomPipe3.x + scrollSpeed
-    if topPipe3.x < (0-topPipe3.width):
+    if topPipe3.x < -87:
         topPipe3.x = WIDTH
+        
+    # updating 6th pipe 
+    bottomPipe3.x = bottomPipe3.x + scrollSpeed
+    if bottomPipe3.x < -87:
         bottomPipe3.x = WIDTH
 
     # bird hits bottom of screen
     if bird.y > HEIGHT:
         # reset
-        print("Game Restarting")
-        bird.x = WIDTH*0.18
-        bird.y = HEIGHT*0.5
-        topPipe1.x = WIDTH*0.33
-        topPipe1.y = 0
-        bottomPipe1.x = WIDTH*0.33
-        bottomPipe1.y = topPipe1.height + gap
-        topPipe2.x = WIDTH*0.66
-        topPipe2.y = -(topPipe2.height*0.2)
-        bottomPipe2.x = WIDTH*0.66
-        bottomPipe2.y = (topPipe2.height + topPipe2.y) + gap
-        topPipe3.x = WIDTH
-        topPipe3.y = -(topPipe3.height*0.4)
-        bottomPipe3.x = WIDTH
-        bottomPipe3.y = (topPipe3.height + topPipe3.y) + gap
-
-    # bird hits 1st pipe group
-    if bird.colliderect(topPipe1) or bird.colliderect(bottomPipe1):
-        print("Game Restarting")
-        bird.x = WIDTH*0.18
-        bird.y = HEIGHT*0.5
-        topPipe1.x = WIDTH*0.33
-        topPipe1.y = 0
-        bottomPipe1.x = WIDTH*0.33
-        bottomPipe1.y = topPipe1.height + gap
-        topPipe2.x = WIDTH*0.66
-        topPipe2.y = -(topPipe2.height*0.2)
-        bottomPipe2.x = WIDTH*0.66
-        bottomPipe2.y = (topPipe2.height + topPipe2.y) + gap
-        topPipe3.x = WIDTH
-        topPipe3.y = -(topPipe3.height*0.4)
-        bottomPipe3.x = WIDTH
-        bottomPipe3.y = (topPipe3.height + topPipe3.y) + gap
-
-    # bird hits 2nd pipe group
-    if bird.colliderect(topPipe2) or bird.colliderect(bottomPipe2):
-        print("Game Restarting")
-        bird.x = WIDTH*0.18
-        bird.y = HEIGHT*0.5
-        topPipe1.x = WIDTH*0.33
-        topPipe1.y = 0
-        bottomPipe1.x = WIDTH*0.33
-        bottomPipe1.y = topPipe1.height + gap
-        topPipe2.x = WIDTH*0.66
-        topPipe2.y = -(topPipe2.height*0.2)
-        bottomPipe2.x = WIDTH*0.66
-        bottomPipe2.y = (topPipe2.height + topPipe2.y) + gap
-        topPipe3.x = WIDTH
-        topPipe3.y = -(topPipe3.height*0.4)
-        bottomPipe3.x = WIDTH
-        bottomPipe3.y = (topPipe3.height + topPipe3.y) + gap
-
-    # bird hits 3rd pipe group
-    if bird.colliderect(topPipe3) or bird.colliderect(bottomPipe3):
-        print("Game Restarting")
-        bird.x = WIDTH*0.18
-        bird.y = HEIGHT*0.5
-        topPipe1.x = WIDTH*0.33
-        topPipe1.y = 0
-        bottomPipe1.x = WIDTH*0.33
-        bottomPipe1.y = topPipe1.height + gap
-        topPipe2.x = WIDTH*0.66
-        topPipe2.y = -(topPipe2.height*0.2)
-        bottomPipe2.x = WIDTH*0.66
-        bottomPipe2.y = (topPipe2.height + topPipe2.y) + gap
-        topPipe3.x = WIDTH
-        topPipe3.y = -(topPipe3.height*0.4)
-        bottomPipe3.x = WIDTH
-        bottomPipe3.y = (topPipe3.height + topPipe3.y) + gap
+        print("Reset!")
+        reset()
+    elif bird.colliderect(topPipe1):
+        # reset
+        print("Reset!")
+        reset()
+    elif bird.colliderect(bottomPipe1):
+        # reset
+        print("Reset!")
+        reset()
+    elif bird.colliderect(topPipe2):
+        # reset
+        print("Reset!")
+        reset()
+    elif bird.colliderect(bottomPipe2):
+        # reset
+        print("Reset!")
+        reset()
+    elif bird.colliderect(topPipe3):
+        # reset
+        print("Reset!")
+        reset()
+    elif bird.colliderect(bottomPipe3):
+        # reset
+        print("Reset!")
+        reset()
 
 # moving
 def on_mouse_down():
     bird.y = bird.y - 50
+
+def reset():
+    # reset
+    bird.x = 160
+    bird.y = 300
+    topPipe1.x = 280
+    bottomPipe1.x = 280
+    topPipe2.x = 545
+    bottomPipe2.x = 545
+    topPipe3.x = 810
+    bottomPipe3.x = 810
 
 # runs everything
 pgzrun.go() 
