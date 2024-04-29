633bd000e2425e90a83ddcad0269bf1f8040befd
Isseymay <35818862+Isseymay@users.noreply.github.com>
2024-04-30 00:17:59 +1000

    Update AdvancedExt1.py

--- AdvancedExt1.py
+++ AdvancedExt1.py
@@ -1,131 +1,125 @@
-#Save highscore in file
-# Making a simple flappy bird game with Pygame Zero
-# flappyBirdV2.1G
-# making a pipe class
-# timer score instead
-# methods for repeated code
-# write to file for highscore 
-# projictile falling
+# Base advanced + Projectile motion extension
 
-# initialise Pygame Zero
+# start modules
 import pgzrun
+import sys
 from random import *
-import math 
 
-# initialize constants
+
+# create constants
 WIDTH = 800
 HEIGHT = 600
-scrollSpeed = -1
-gameOver = False
 score = 0
-entered = False
+gameOver = False
 
-class pipeGroup():
-    def __init__(self,x):
+
+# print welcome
+print('''The game is about to start!
+Click the mouse to "flap" upwards
+Dodge the pipes and the floor
+Good luck and have fun!''')
+
+
+# make background
+background = Actor("bg")
+background.x = 400
+background.y = 300
+
+
+# make bird
+bird = Actor("bird")
+bird.x = 160
+bird.y = 300
+t = 0
+y0 = bird.y
+g = 9.81
+
+
+# make pipes
+class Pipes():
+    def __init__(self, x):
+        gap = randint(160,260)
+        y = randint((300-250+(gap//2)),(850-300-(gap//2)))
+
+
         self.top = Actor("top")
         self.top.x = x
-        self.top.y = randint(int(-(self.top.height*0.3)),int(self.top.height*0.1))
+        self.top.y = y - (300 + (gap // 2))
         self.bottom = Actor("bottom")
         self.bottom.x = x
-        self.bottom.y = (self.top.height+self.top.y) + randint(minGap,maxGap)
+        self.bottom.y = y + 300 + (gap // 2)
 
-    def updatePipes(self,bird):
-        global gameOver,score
+    def updatePipes(self, bird):
+        global score, gameOver
+        self.top.x = self.top.x - 1
+        self.bottom.x = self.bottom.x - 1
+        if self.top.x < -44:
+            self.top.x = 844
+            self.bottom.x = 844
+            gap = randint(160,260)
+            y = randint((300-250+(gap//2)),(850-300-(gap//2)))
+            self.top.y = y - (300 + (gap//2))
+            self.bottom.y = y + 300 + (gap//2)
+
         if bird.colliderect(self.top) or bird.colliderect(self.bottom):
+            print("Game Over!")
+            print(f"Your score was {score}")
             gameOver = True
-        self.top.x = self.top.x-2
-        self.bottom.x = self.bottom.x-2
-        if self.top.x < (0-(self.top.width//2)):
-            self.top.x = WIDTH
-            self.bottom.x = WIDTH
-            self.top.y = randint(int(-(self.top.height*0.3)),int(self.top.height*0.1))
-            self.bottom.y = (self.top.height+self.top.y) + randint(minGap,maxGap)
-            if not gameOver:
-                score = score+1
 
-    
+
     def drawPipes(self):
         self.top.draw()
         self.bottom.draw()
 
 
 
-# make bird
-bird = Actor("bird")
-bird.x = WIDTH*0.15
-bird.y = HEIGHT*0.5
-originaly = bird.y
-t=0
-v0 = 80
-minGap = int((bird.height*2.5)+50)
-maxGap = int((bird.height*3.5)+50)
-
-pipes1 = pipeGroup(WIDTH*0.4)
-pipes2 = pipeGroup(WIDTH*0.75)
-pipes3 = pipeGroup(WIDTH*1.1)
-pipeList=[pipes1,pipes2,pipes3]
+pipes1 = Pipes(266)
+pipes2 = Pipes(532)
+pipes3 = Pipes(798)
+pipes = [pipes1,pipes2,pipes3]
 
 # draw everything to screen
 def draw():
-    global entered
-    if gameOver:
-        screen.clear()
+    if gameOver == True:
         screen.fill((0,0,0))
-        outtext = scoring()
-        screen.draw.text(f"Game Over\n{outtext}",center=(WIDTH*0.5,HEIGHT*0.5),align="left",color=(255,255,255), fontsize = 60)
+        screen.draw.text(f"Game Over!\n Your score was {score}", center = (400,300), fontsize = 60)
     else:
-        # set background image
-        screen.blit("bg",(0,0))
+        # draw background
+        background.draw()
+
+
+        # draw characters
         bird.draw()
-
-        for pipes in pipeList:
-            pipes.drawPipes()
-        screen.draw.text(f"Score: {score}",topleft=(10,10),color=(0,0,0), fontsize = 30)
+        for pipe in pipes:
+            pipe.drawPipes()
 
 
-# updates everything
+# update everything
 def update():
-    global score,gameOver,t,originaly,v0
-    t+=0.1
-    velocity = v0 * math.sin(math.radians(0))
-    # updating bird
-    bird.y = originaly - (velocity*t - (0.5*9.81*(t**2)))
+    global score, gameOver, t
+    if gameOver == False:
+        t = t + 0.1
+        # update bird
+        bird.y = y0 + (0.5 * g * (t**2))
 
-    # updating pipes
-    for pipes in pipeList:
-        pipes.updatePipes(bird)
 
-    # bird hits bottom of screen
-    if bird.y > HEIGHT:
-        # reset
-        gameOver = True
+        # update pipes
+        for pipe in pipes:
+            pipe.updatePipes(bird)
+        # bird hits bottom of screen
+        if bird.y > HEIGHT:
+            print("Game Over!")
+            print(f"Your score was: {score}")
+            gameOver = True
+
 
 # moving
 def on_mouse_down():
-    global t,originaly
+    global t, y0
     bird.y = bird.y - 50
-    originaly = bird.y
     t = 0
-
-def scoring():
-    global score,entered
-    highscore = 0
-    for line in open(".\GPN\Flappy_Bird\Highscore.txt","r"):
-        line = line.strip()
-        if int(line)> highscore:
-            highscore = int(line)
-    if score > highscore:
-        file = open(".\GPN\Flappy_Bird\Highscore.txt","a")
-        if not entered:
-            file.writelines(str(score)+"\n")
-            entered  = True
-        file.close()
-        return(f"You beat the highscore!\n Your score was: {score}")
-
-    else:
-        return(f"Your score was: {score}\n The current highscore is: {highscore}")
-
+    y0 = bird.y
 
 
 # runs everything
-pgzrun.go() 
+pgzrun.go()
